4c1341a31b2da58c638bf46ce19690f6993f147a
mridulmahajan04 <mridulmahajan16@gmail.com>
2025-07-08 23:04:00 +0530

    Batch Prediction

--- app.py
+++ app.py
@@ -10,14 +10,15 @@ from pymongo import MongoClient
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.pipeline.training_pipeline import TrainingPipeline
-
+from networksecurity.utils.main_utils.ml_utils.model.estimator import NetworkModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile, Request
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 import pandas as pd
-
+from fastapi.templating import Jinja2Templates
+templates=Jinja2Templates(directory="./templates")
 from networksecurity.utils.main_utils.utils import load_object
 from networksecurity.constants.training_pipeline import (
     DATA_INGESTION_COLLECTION_NAME,
@@ -49,3 +50,23 @@ async def train_route():
         return Response("Training is Successfull")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+@app.post("/predict")
+async def predict_route(request:Request, file:UploadFile=File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        preprocessor=load_object("final_model/preprocessor.pkl")
+        final_model=load_object("final_model/model.pkl")
+        network_model=NetworkModel(preprocessor=preprocessor, model=final_model)
+        print(df.iloc[0])
+        y_pred=network_model.predict(df)
+        print(y_pred)
+        df['predicted_column']=y_pred
+        df.to_csv("predict_output/output.csv")
+        table_html=df.to_html(classes='table table-striped')
+        return templates.TemplateResponse('table.html', {"request":request, "table":table_html})
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+if __name__ == "__main__":
+    app_run(app, host="localhost", port=8000)
